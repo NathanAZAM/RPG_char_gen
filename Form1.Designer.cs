91759209d68063387b84e4d6070cb9ce30e50974
Nathan AZAM <nathan-a@live.fr>
2020-10-09 13:13:54 +0200

    Beggining of project, now fills a (pretty bad) template with player's name

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Tbox_name = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AccessibleDescription = "Your character name";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your character name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Tbox_name
+            // 
+            this.Tbox_name.AccessibleDescription = "Name of your character";
+            this.Tbox_name.AccessibleName = "Enter the name of your character";
+            this.Tbox_name.Location = new System.Drawing.Point(15, 26);
+            this.Tbox_name.MaxLength = 255;
+            this.Tbox_name.Name = "Tbox_name";
+            this.Tbox_name.Size = new System.Drawing.Size(301, 20);
+            this.Tbox_name.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 236);
+            this.button1.Name = "button_generate";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Main_window
             // 
@@ -36,14 +70,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(441, 290);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Tbox_name);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Main_window";
             this.Text = "RPG character generator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Tbox_name;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+// Word
+using Word = Microsoft.Office.Interop.Word;
+using System.Reflection;
 
 namespace RPG_char_gen
 {
@@ -15,6 +18,49 @@ namespace RPG_char_gen
         public Main_window()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            object oMissing = System.Reflection.Missing.Value;
+            object oEndOfDoc = "\\endofdoc"; /* \endofdoc is a predefined bookmark */
+
+            //Start Word and create a new document.
+            //Microsoft.Office.Interop.Word.Application oWord;
+            //Microsoft.Office.Interop.Word.Document oDoc;
+            //oWord = new Microsoft.Office.Interop.Word.Application();
+            //oWord.Visible = true;
+            //oDoc = oWord.Documents.Add(ref oMissing, ref oMissing,
+            //ref oMissing, ref oMissing);
+
+            //Open template
+            Word.Application oWord;
+            Word.Document oDoc;
+            object oTemplate = "D:\\Projets\\Cpp\\RPG_char_gen\\bin\\Debug\\Templates\\Joueur.dotx";
+            oWord = new Word.Application();
+            //displays word doc after generation
+            oWord.Visible = true;
+            ///TODO : add an assert if we dont find the doc
+            oDoc = oWord.Documents.Add(ref oTemplate, ref oMissing,
+            ref oMissing, ref oMissing);
+
+            ///TODO : condition not working, have to find a isEmpty()
+            if (Tbox_name.Text.Length < 1 )
+                MessageBox.Show("Character name empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            else
+            {
+                object playername_bookmark = "player_name";
+                oDoc.Bookmarks[ref playername_bookmark].Range.Text = Tbox_name.Text;
+            }
+
+            //Close this form.
+            this.Close();
         }
     }
 }
